99ba2a889046d64cd6abefb0b21b819d4384ab6b
debasishbihari <debasisbihari287@gmail.com>
2023-02-21 14:07:04 +0530

    added User Model

---
+++ backend/models/User.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    confirm_email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    confirm_password:{
+        type: String,
+        required: true
+    },
+    mobile:{
+        type: Number,
+        required: true
+    },
+    refer_code:{
+        type: String,
+        required: true
+    },
+    loyalty:{
+        type: String,
+        required: true
+    }
+},{
+    versionKey : false
+})
+
+const UserModel = mongoose.model("user", userSchema)
+
+module.exports = {
+    UserModel
+}
+
