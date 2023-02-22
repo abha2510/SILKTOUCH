a930b310294f841303e32b27333b3834a54c0214
shivam malik <shivammalik962@gmail.com>
2023-02-22 22:54:15 +0530

    admin-panel-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,14 +8,19 @@ import {ChakraProvider} from "@chakra-ui/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
+
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </ChakraProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// {/* <Routes>
+//       <Route path="/checkout" element={<Checkout/>}></Route>
+//      </Routes> */}
