a09e8ac644c3f308ae4ab4048b9bec4006e62134
Ayush Sinha <sinhamitali713@gmail.com>
2023-02-24 19:26:45 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@
 
 import {Routes,Route} from "react-router-dom"
 import './App.css';
-//import Navbar from "./components/Navbar";
 import Checkout from "./pages/Checkout";
 import Home from "./pages/Home"
+import Mobiles from "./categoryFolder/allRoute/mobiles/mobiles";
 
 function App() {
   return (
     <div className="App">
+      
      <Routes>
      <Route path="/" element={<Home/>}></Route>
      <Route path="/checkout" element={<Checkout/>}></Route>
