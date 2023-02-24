c989a4a4220c4bb94154a89b60a867b0f502faf7
murari_kaushal <murari.kaushal@bizbrolly.com>
2023-02-24 20:01:19 +0530

    day4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,16 @@ import {Routes,Route} from "react-router-dom"
 import './App.css';
 //import Navbar from "./components/Navbar";
 import Checkout from "./pages/Checkout";
+import Page from "./pages/Page"
+import CartPage from "./pages/CartPage"
 
 function App() {
   return (
     <div className="App">
      <Routes>
       <Route path="/checkout" element={<Checkout/>}></Route>
+       <Route path="/page" element={<Page/>}></Route>
+        <Route path="/cartpage" element={<CartPage/>}></Route>
      </Routes>
     </div>
 
