ba84d37c8f013d3a792d7ce05077aac74c7257a8
Abha Meshram <abha25meshram@gmail.com>
2023-02-24 22:12:02 +0530

    add env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import AllRoutes from "./AllRoutes/AllRoutes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
     <AllRoutes/>
+    <Footer/>
     </div>
-
 
 
   
