a72768cdfbfe889b537e2c426379e88659b54da1
Abha Meshram <abha25meshram@gmail.com>
2023-02-26 21:15:30 +0530

    day4

--- frontend/src/AllRoutes/AllRoutes.jsx
+++ frontend/src/AllRoutes/AllRoutes.jsx
@@ -11,12 +11,13 @@ import Register from '../pages/register/Register'
 import Login from '../pages/login/Login'
 import Home from "../pages/Home";
 import Checkout from "../pages/Checkout";
-import Bestsellers, { Iron } from '../categoryFolder/allRoute/Iron/Bestseller'
-import Page from '../pages/Page';
+import Bestsellers from '../categoryFolder/allRoute/Iron/Bestseller'
+
 import CartPage from "../pages/CartPage";
-import Mobiles from "../categoryFolder/allRoute/mobiles/mobiles"
 import Makeup from '../categoryFolder/allRoute/makeup/makeup'
 import Bath_Body from '../categoryFolder/allRoute/juicers/bath&body'
+import BrowserBy from '../categoryFolder/allRoute/BrowserBy/BrowserBy'
+import Page from '../pages/Page'
 
 const AllRoutes = () => {
   return (
@@ -26,7 +27,7 @@ const AllRoutes = () => {
    <Route path="/" element={<Home/>}></Route>
    <Route path="/checkout" element={<Checkout/>}></Route>
    <Route path="/bestseller" element={<Bestsellers/>}></Route>
-   <Route path="/mobiles" element={<Mobiles/>}></Route>
+   <Route path="/browserby" element={<BrowserBy/>}></Route>
    <Route path="/makeup" element={<Makeup/>}></Route>
    <Route path="/bath&beauty" element={<Bath_Body/>}></Route>
    <Route path="/page" element={<Page/>}></Route>
--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,110 +1,179 @@
 import { border, Box, Button, Flex, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineHeart } from "react-icons/ai";
 import { GiJusticeStar } from "react-icons/gi";
 import { FaGooglePay } from "react-icons/fa";
 import { SiPaytm } from "react-icons/si";
 import { GrPaypal } from "react-icons/gr";
+import { Link } from 'react-router-dom';
+
 const CartPage = () => {
+    const [value,setValue]=useState(1);
+   const [shop,setshop] = useState([])
+   const [total,settotal]=useState(0)   
+
+
+ const totalSum =()=>{
+
+  let newsum = shop.reduce((aq,current)=>{
+   aq+=  current.mrp
+
+   return aq
+   
+  },0)
+  return newsum
+
+ }
+
+ const handleSub=()=>{
+  if(value===0){
+      return;
+  }
+  setValue(value-1)
+};
+const handleAdd=()=>{
+  setValue(value+1)
+}
+//  totalSum()
+useEffect(()=>{
+  let ttol = totalSum()
+  settotal (ttol)
+},[])
+  useEffect(()=>{
+
+    fetch("https://prickly-beret-goat.cyclic.app/carts",{
+       headers:{
+        "Authorization":localStorage.getItem("token")
+       },
+       
+   })
+   .then(res=>res.json())
+   .then(res=>
+    setshop(res)
+   )
+
+//  console.log(total,"total")
+ },[])
+
+
+
+
   return (
-    <div>
-            <Box border={"0px solid black"}  p={5}> 
-            <div> <hr></hr></div>
-        <Flex  >
-          <Flex gap={25} justifyContent={"left"}  w={"50%"} mt={4} >
-          <text>item</text>
-          </Flex>
-          
-        {/* </Flex>
-        <Flex  gap={55} justifyContent={"space-around"} w={"100%"} bgColor="red.100" ml={"50%"}>  */}
-         <Flex w={"40%"} justifyContent={"space-around"} gap={25} mt={4} >
-         <text >price</text>
-           <text>quantity</text>
-           <text>subtotal</text>
-         </Flex>
-           </Flex>
-        <Box mt={4}> <hr></hr></Box>
-            </Box>
-
-
-
-
-
-
-
-
-
-
-
-
-            <Box border={"0px solid black"}  p={5}> 
-          
-        <Flex  >
-          <Flex gap={25} justifyContent={"left"}  w={"50%"}  >
-         <Image w={"20%"} src="https://static.thcdn.com/images/large/webp//productimg/1600/1600/12915557-2954917112863726.jpg"></Image>
-            <Box>
-         <Text mt={3}>Oribe Gold Lust Repair Restore Shampoo (8.5 fl. oz.)</Text>
-         <Text textAlign={"left"}>In stock</Text>
-          <Flex mt={5} gap={2}>
-
-         <AiOutlineHeart/>
-         <Text mt={-1.5}>Save to My Favorites</Text>
-          </Flex>
-
-            </Box>
-          </Flex>
-          
-        {/* </Flex>
-        <Flex  gap={55} justifyContent={"space-around"} w={"100%"} bgColor="red.100" ml={"50%"}>  */}
-         <Flex w={"50%"} justifyContent={"space-around"} gap={25} mt={4} >
-         <text >$53.00</text>
-                    <Flex gap={1}>
-                       <Button>-</Button>
-                       <Button>1</Button>
-                       <Button>+</Button>
-                    </Flex>
-         <text >$53.00</text>
-         <Button>X</Button>
-         </Flex>
-           </Flex>
-        <Box mt={4}> <hr></hr></Box>
-            </Box>
+ 
      
+    <div>
+  {/* {
+    shop.map((el)=>{
+      return <div key={el._id}>
+     <h1>{el.name}</h1>
+     <img src={el.img_url_1} alt="" />
+     <h3>{el.mrp}</h3>
+
+<h4>{total}</h4>
+      </div>
+    })
+  }
+        */}
+
+
+
+{
+ shop.map((el)=>{
+  return <div key={el._id}>
+
+<Box border={"0px solid black"}  p={5}> 
+            
+<div> <hr></hr></div>
+<Flex  >
+<Flex gap={25} justifyContent={"left"}  w={"50%"} mt={4} >
+<text>item</text>
+</Flex>
+
+<Flex w={"40%"} justifyContent={"space-around"} gap={25} mt={4} >
+<text >price</text>
+<text>quantity</text>
+<text>subtotal</text>
+</Flex>
+</Flex>
+<Box mt={4}> <hr></hr></Box>
+</Box>
+
+
+<Box border={"0px solid black"}  p={5}> 
+
+<Flex  >
+<Flex gap={25} justifyContent={"left"}  w={"50%"}  >
+<Image w={"20%"} src={el.img_url_1}></Image>
+<Box>
+<Text mt={3}>Oribe Gold Lust Repair Restore Shampoo (8.5 fl. oz.)</Text>
+<Text textAlign={"left"}>In stock</Text>
+<Flex mt={5} gap={2}>
+
+<AiOutlineHeart/>
+<Text mt={-1.5}>Save to My Favorites</Text>
+</Flex>
+
+</Box>
+</Flex>
+
+
+<Flex w={"50%"} justifyContent={"space-around"} gap={25} mt={4} >
+<text >${el.mrp}</text>
+        <Flex gap={1}>
+           <Button disabled={value===0} onClick={()=>handleSub(1)}>-</Button>
+           <Button>{value}</Button>
+           <Button disabled={value===0} onClick={()=>handleAdd(1)}>+</Button>
+        </Flex>
+<text >${Math.floor(value*el.mrp)}</text>
+<Button>X</Button>
+</Flex>
+</Flex>
+<Box mt={4}> <hr></hr></Box>
+</Box>
 
 
 
 
 
-            <Box border={"0px solid black"}  p={5}> 
-           
-        
-          
-        {/* </Flex>
-        <Flex  gap={55} justifyContent={"space-around"} w={"100%"} bgColor="red.100" ml={"50%"}>  */}
-         <Flex w={"100%"} justifyContent={"space-around"} gap={25} mt={4} >
-          <Flex gap={25}> 
-           < GiJusticeStar/>
-           <Text mt={-1}>Rewards members earn 265 points on this order</Text>
-          </Flex>
-           <Flex gap={25}>
-            <Text>Cart Subtotal:</Text>
-            <Text>$550</Text>
-           </Flex>
-         </Flex>
-          
-        <Box mt={4}> <hr></hr></Box>
-            </Box>
+
+<Box border={"0px solid black"}  p={5}> 
 
 
 
-            <Flex justifyContent={"space-around"}>
-               <button style={{border:"1px solid black",padding:"5px"}}>Continue Shoping</button>
-             <Flex gap={20} size={"20px"} >
-             <FaGooglePay size={"20px"}/>
-              < GrPaypal />
-              <SiPaytm/>
-             </Flex>
-            </Flex>
+
+<Flex w={"100%"} justifyContent={"space-around"} gap={25} mt={4} >
+<Flex gap={25}> 
+< GiJusticeStar/>
+<Text mt={-1}>Rewards members earn 265 points on this order</Text>
+</Flex>
+<Flex gap={25}>
+<Text>Cart Subtotal:</Text>
+<Text>${el.total}</Text>
+</Flex>
+</Flex>
+
+<Box mt={4}> <hr></hr></Box>
+</Box>
+
+
+
+<Flex justifyContent={"space-around"}>
+  <Link to="/checkout">
+   <button style={{border:"1px solid black",padding:"5px"}}>Checkout</button>
+   </Link>
+ <Flex gap={20} size={"20px"} >
+ <FaGooglePay size={"50px"}/>
+  < GrPaypal size={"50px"}/>
+  <SiPaytm size={"50px"}/>
+ </Flex>
+</Flex>
+
+</div>
+})
+
+
+}
+
 
     </div>
   )
